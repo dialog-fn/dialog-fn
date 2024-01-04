f47d01f2bd740f296f6b575d6cede5a2dc40bebb
njacob1001 <jacob.gonzalez.co@gmail.com>
2024-01-04 09:37:14 -0500

    build: fix package.json

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -5,12 +5,20 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [
+    react(),
+    dts({
+      include: ["lib", "src"],
+      rollupTypes: true,
+      compilerOptions: { declaration: true },
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
+      fileName: "main",
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
