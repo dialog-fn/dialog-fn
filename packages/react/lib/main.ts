520d312d2cb9a618f98ed0405b04e645b2a849b9
njacob1001 <jacob.gonzalez.co@gmail.com>
2024-02-09 16:56:59 -0500

    fix: type declaration

--- packages/react/lib/main.ts
+++ packages/react/lib/main.ts
@@ -1,2 +1,8 @@
-export type { DialogComponentProps as DialogProps } from "@dialog-fn/core";
 export { createDialog } from "../src/library";
+
+export interface DialogProps<T = any, K = any> {
+  isOpen?: boolean;
+  data?: T;
+  onClose?: () => void;
+  onConfirm?: (response?: K) => void;
+}
