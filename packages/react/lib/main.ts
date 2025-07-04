fe245c13f6e33953d66a833fbb5d9cb7e66d133c
njacob1001 <njacob1001@gmail.com>
2025-07-04 06:36:19 -0500

    update types

--- packages/react/lib/main.ts
+++ packages/react/lib/main.ts
@@ -1,8 +1,9 @@
 export { createDialog } from "../src/library";
 
-export interface DialogProps<T = any, K = any> {
+export interface DialogProps<T = any, K = any, S=any> {
   isOpen?: boolean;
   data?: T;
   onClose?: () => void;
   onConfirm?: (response?: K) => void;
+  status?: S;
 }
