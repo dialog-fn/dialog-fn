1d7c3c43a057cc0083de5f3788fd9f2140f231ae
njacob1001 <njacob1001@gmail.com>
2025-07-04 07:10:23 -0500

    feat: export type

--- packages/react/lib/main.ts
+++ packages/react/lib/main.ts
@@ -1,4 +1,5 @@
 export { createDialog } from "../src/library";
+export type { DialogComponentProps } from '../src/library'
 
 export interface DialogProps<T = any, K = any, S=any> {
   isOpen?: boolean;
